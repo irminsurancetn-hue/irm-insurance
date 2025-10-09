ecc255a7c2b58ac38695745672af89ea007dec38
Abdul Hanan <webbdevabdul@gmail.com>
2025-10-10 00:14:28 +0500

    Fix nodemailer typo: createTransporter -> createTransport

--- api/send-email.js
+++ api/send-email.js
@@ -13,7 +13,7 @@ export default async function handler(req, res) {
 
   try {
     // Create transporter using Gmail SMTP
-    const transporter = nodemailer.createTransporter({
+    const transporter = nodemailer.createTransport({
       service: 'gmail',
       auth: {
         user: process.env.EMAIL_USER, // webbdevabdul@gmail.com
